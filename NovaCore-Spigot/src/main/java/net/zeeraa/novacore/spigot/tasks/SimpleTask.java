310ece3d82df9fac4ed61b5771ee76b4e84f86bc
Anton udén <udenanton@hotmail.com>
2023-02-24 19:58:32 +0100

    allow update of SimpleTask time without creating a new SimpleTask

--- NovaCore-Spigot/src/main/java/net/zeeraa/novacore/spigot/tasks/SimpleTask.java
+++ NovaCore-Spigot/src/main/java/net/zeeraa/novacore/spigot/tasks/SimpleTask.java
@@ -83,6 +83,31 @@ public class SimpleTask extends Task {
 		this.taskExecutionMode = taskExecutionMode;
 	}
 
+	/**
+	 * Update the interval the task run at. This also calls
+	 * {@link SimpleTask#restart()} if the task is running
+	 * 
+	 * @param delay The delay
+	 */
+	public void updateInterval(long delay) {
+		this.updateInterval(delay, delay);
+	}
+
+	/**
+	 * Update the interval the task run at. This also calls
+	 * {@link SimpleTask#restart()} if the task is running
+	 * 
+	 * @param delay  The delay
+	 * @param period The period
+	 */
+	public void updateInterval(long delay, long period) {
+		this.delay = delay;
+		this.period = period;
+		if (isRunning()) {
+			restart();
+		}
+	}
+
 	@Override
 	public boolean start() {
 		if (bukkitTask != null) {
