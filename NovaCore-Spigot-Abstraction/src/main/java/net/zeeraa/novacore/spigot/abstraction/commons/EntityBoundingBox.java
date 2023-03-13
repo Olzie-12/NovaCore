4641637baa15c67641d0c4353b91e6401d065e4e
Anton udén <udenanton@hotmail.com>
2023-03-13 23:29:33 +0100

    Update EntityBoundingBox.java

--- NovaCore-Spigot-Abstraction/src/main/java/net/zeeraa/novacore/spigot/abstraction/commons/EntityBoundingBox.java
+++ NovaCore-Spigot-Abstraction/src/main/java/net/zeeraa/novacore/spigot/abstraction/commons/EntityBoundingBox.java
@@ -1,21 +1,19 @@
 package net.zeeraa.novacore.spigot.abstraction.commons;
 
 public class EntityBoundingBox {
+	private final float height;
+	private final float width;
 
-    private final float height;
-    private final float width;
+	public EntityBoundingBox(float height, float width) {
+		this.height = height;
+		this.width = width;
+	}
 
-    public EntityBoundingBox(float height, float width) {
-        this.height = height;
-        this.width = width;
-    }
+	public float getHeight() {
+		return height;
+	}
 
-    public float getHeight() {
-        return height;
-    }
-
-    public float getWidth() {
-        return width;
-    }
-
+	public float getWidth() {
+		return width;
+	}
 }
